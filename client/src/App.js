c00dc25f5ad7893869c380cf6ddb04b5674e2cb8
clowery <clowery52916@gmail.com>
2018-05-07 13:33:46 -0400

    updating nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/projects" component={Projects} />
-          <Route exact path="/resume" component={Resume}/>
           <Route exact path='/about' component={About}/>
+          <Route exact path="/resume" component={Resume}/>
           <Route exact path='/contact' component={Contact}/>
           <Route exact path='/genba-on-the-go' component={Genba}/>
         </Switch>
